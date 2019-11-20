481f02b0414b713c8cc822e4741dc493703caf0e
Dinu Buzut <50601975+DinuB@users.noreply.github.com>
2019-11-20 17:34:58 -0300

    Update products.js

--- src/js/products.js
+++ src/js/products.js
@@ -1,6 +1,7 @@
 var xhr = new XMLHttpRequest();
 var products;
 
+// CHAMAR API JSON
 xhr.open('GET', 'https://cdn.rawgit.com/LucasRuy/1d4a5d45e2ea204d712d0b324af28bab/raw/342e0e9277be486102543c7f50ef5fcf193234b6/potions.json');
 xhr.send(null);
 
@@ -10,9 +11,6 @@ xhr.onreadystatechange = function(){
       listaProdutos();
     }
 }
-
-// DECLARAR VARIAVEL potion QUE TOMA O VALOR DE O OBJECTO
-// pottions DENTRO DO JSON
 
 // lISTAGEM DE PRODUTOS
 function listaProdutos(){
